625c65af8ee16807561682b4d64ccd8d5a7ed1c7
DeMarkiz <markberezhnoy27@gmail.com>
2025-01-25 16:35:45 +0300

    31. Права доступа в DRF, исправил

--- lms/serializers.py
+++ lms/serializers.py
@@ -15,9 +15,13 @@ class LessonSerializer(ModelSerializer):
 class CourseSerializer(ModelSerializer):
     lessons_count = SerializerMethodField()
     lessons = LessonSerializer(many=True)
+    is_subscribed = SerializerMethodField()
 
     def get_lessons_count(self, course):
         return course.lessons.count()
+
+    def get_is_subscribed(self, instance):
+        return instance.subscriptions.filter(user=self.context['request'].user).exists()
 
     class Meta:
         model = Course
@@ -29,4 +33,5 @@ class CourseSubscriptionSerializer(ModelSerializer):
     class Meta:
         model = CourseSubscription
         fields = ["course"]
+        read_only_fields = ('user',)
         read_only_fields = ('user',)
