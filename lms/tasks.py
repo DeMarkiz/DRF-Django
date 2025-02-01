22cf7dd927e2ec0a67100ba85df97bf4ad06c626
DeMarkiz <markberezhnoy27@gmail.com>
2025-02-01 11:39:54 +0300

    33 Celery

--- lms/tasks.py
+++ lms/tasks.py
@@ -1,26 +1,31 @@
 from celery import shared_task
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.core.mail import send_mail
-from django.utils.timezone import now, timedelta
-from .models import Course, CourseSubscription
+from lms.models import Course
+from config.settings import DEFAULT_FROM_EMAIL
 
 
 @shared_task
 def send_course_update_notifications(course_id):
-    course = Course.objects.get(id=course_id)
-    last_updated_threshold = now() - timedelta(hours=4)
+    """Отправка подписчикам уведомлений об изменениях в курсе"""
+    course = get_object_or_404(Course, id=course_id)
+    subscriptions = course.course_subscription.all()
+    recipient_list = [sub.user.email for sub in subscriptions]
+    subject = f"Курс {course.name} обновлен!"
+    message = f'Здравствуйте! Сообщаем, что в курсе "{course.name}" обновления!'
 
-    if course.updated_at < last_updated_threshold:
-        return "Курс недавно обновился, обновления не отправлены."
+    from_email = DEFAULT_FROM_EMAIL
 
-    subscription = CourseSubscription.objects.filter(course=course)
-    emails = [sub.user.email for sub in subscription]
+    # Отправка письма
+    responses = {}
 
-    if emails:
-        send_mail(
-            subject='Обновление курса',
-            message=f'Материалы курса {course.title} были обновлены.',
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=emails
-        )
-        return 'Уведомления отправлены'
+    for recipient in recipient_list:
+        try:
+            send_mail(subject, message, from_email, [recipient])
+
+            responses[recipient] = "Успешно отправлено"
+        except Exception as e:
+            response = f"{recipient}: Ошибка: {str(e)}"
+            responses[recipient] = f"Ошибка: {str(e)}"
+
+    print(responses)
