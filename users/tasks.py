fdb1587a283db7fe9684396352bb1047b32e12df
DeMarkiz <markberezhnoy27@gmail.com>
2025-02-03 00:00:09 +0300

    33 Celery дополнил

--- users/tasks.py
+++ users/tasks.py
@@ -5,9 +5,9 @@ from users.models import CustomUser
 from datetime import timedelta
 
 
+
 @shared_task
 def user_block():
     users = CustomUser.objects.filter(last_login__lt=now() - timedelta(days=30))
-    if users.count() > 0:
+    if users.exists():  # Проверяем, есть ли такие пользователи
         users.update(is_active=False)
-        users.save()
